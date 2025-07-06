cc04c0f974022f3a690c872c2a66cf5abe02e58b
Ryan Shang <50010920+ryqdev@users.noreply.github.com>
2025-07-06 12:02:46 +0800

    refactor: use deck as an abstraction

--- lib/game.ml
+++ lib/game.ml
@@ -16,6 +16,19 @@ type card =
     ; suit: card_suit
     }
 
+type deck =
+    { cards: card array
+     ; card_index: int ref
+    }
+
+let deal_top_card deck =
+    if !(deck.card_index) < Array.length deck.cards then
+        let card = deck.cards.(!(deck.card_index)) in
+        incr deck.card_index;
+        Some card
+    else
+        None
+
 type player_position =
     | BTN
     | SB
@@ -83,49 +96,59 @@ let string_of_player player =
         card_str
         (string_of_int player.chips)
 
-let shuffle_cards cards = cards
+let shuffle_cards deck = deck
 
-let deal_cards_to_player cards players =
-    let card_index = ref 0 in
+let deal_cards_to_player deck players =
     let deal_round players round_count =
         Printf.printf "Dealing cards in round %d" round_count;
         List.map (fun p ->
-            let card = cards.(!card_index) in
-            incr card_index;
-            Printf.printf "%s gets %s\n" p.name (string_of_card card);
-            let updated_hole_cards = match round_count with
-                | 1 -> Some (card, card)
-                | 2 -> (match p.hole_cards with
-                    | Some (first_card, _) -> Some (first_card, card)
-                    | None -> failwith "Player should have received first card in round 1"
-                )
-                | _ -> failwith ("Invalid round count: " ^ string_of_int round_count)
-            in
-            {p with hole_cards = updated_hole_cards}
+            match deal_top_card deck with
+            | Some (card) ->
+                Printf.printf "%s gets %s\n" p.name (string_of_card card);
+                let updated_hole_cards = match round_count with
+                    | 1 -> Some (card, card)
+                    | 2 -> (match p.hole_cards with
+                        | Some (first_card, _) -> Some (first_card, card)
+                        | None -> failwith "Player should have received first card in round 1"
+                    )
+                    | _ -> failwith ("Invalid round count: " ^ string_of_int round_count)
+                in
+                {p with hole_cards = updated_hole_cards}
+            | None -> failwith "No Card in Deck"
         ) players
     in
     let round1 = deal_round players 1 in
     let round2 = deal_round round1 2 in
     round2
 
+
+(* let deal_community_cards cards card_index = *)
+(*    Printf.printf "Dealing community cards...\n"; *)
+
+
 let create player_num =
     Printf.printf "Creating game with %d players\n" player_num;
     let suits = [Spade; Heart; Club; Diamond] in
     let values = [Ace; Number 2; Number 3; Number 4; Number 5; Number 6;
                     Number 7; Number 8; Number 9; Number 10; Jack; Queen; King] in
-    let cards = List.map (fun suit ->
-        List.map (fun value -> {value; suit}) values
-        ) suits
-    |> List.concat
-    |> Array.of_list in
 
-    Array.iter (fun card -> Printf.printf "%s " (string_of_card card)) cards;
+    let deck = {
+        cards = List.map (fun suit ->
+                        List.map (fun value -> {value; suit}) values
+                        ) suits
+                    |> List.concat
+                    |> Array.of_list;
+        card_index = ref 0;
+    } in
+
+
+    Array.iter (fun card -> Printf.printf "%s " (string_of_card card)) deck.cards;
     Printf.printf "\n";
 
-    let cards = shuffle_cards cards in
+    let shuffled_deck = shuffle_cards deck in
 
     Printf.printf "After shuffling: \n";
-    Array.iter (fun card -> Printf.printf "%s " (string_of_card card)) cards;
+    Array.iter (fun card -> Printf.printf "%s " (string_of_card card)) shuffled_deck.cards;
     Printf.printf "\n";
 
     let players = [
@@ -139,7 +162,7 @@ let create player_num =
 
     List.iter (fun p -> Printf.printf "%s \n" (string_of_player p)) players;
 
-    let players_with_cards = deal_cards_to_player cards players in
+    let players_with_cards = deal_cards_to_player deck players in
 
     Printf.printf "Players after dealing:\n";
 
