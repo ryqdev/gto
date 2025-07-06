9dfc5516a2ab4cb5da2fffe2d3a6541e668287ed
Ryan Shang <50010920+ryqdev@users.noreply.github.com>
2025-07-06 09:46:17 +0800

    chore: format

--- lib/game.ml
+++ lib/game.ml
@@ -17,30 +17,30 @@ type card =
     }
 
 let string_of_card_value = function
-  | Ace -> "A"
-  | King -> "K"
-  | Queen -> "Q"
-  | Jack -> "J"
-  | Number n -> string_of_int n
+    | Ace -> "A"
+    | King -> "K"
+    | Queen -> "Q"
+    | Jack -> "J"
+    | Number n -> string_of_int n
 
 let color_reset = "\027[0m"
 let color_red = "\027[31m"
 let color_white = "\027[37m"
 
 let string_of_card_suit = function
-  | Spade -> color_white ^ "♠" ^ color_reset
-  | Heart -> color_red ^ "♥" ^ color_reset
-  | Club -> color_white ^ "♣" ^ color_reset
-  | Diamond -> color_red ^ "♦" ^ color_reset
+    | Spade -> color_white ^ "♠" ^ color_reset
+    | Heart -> color_red ^ "♥" ^ color_reset
+    | Club -> color_white ^ "♣" ^ color_reset
+    | Diamond -> color_red ^ "♦" ^ color_reset
 
 (* let string_of_card_suit = function *)
-(*  | Spade -> "♠" *)
-(*  | Heart -> "♥" *)
-(*  | Diamond -> "♦" *)
-(*  | Club -> "♣" *)
+(*    | Spade -> "♠" *)
+(*    | Heart -> "♥" *)
+(*    | Diamond -> "♦" *)
+(*    | Club -> "♣" *)
 
 let string_of_card card =
-  (string_of_card_value card.value) ^ (string_of_card_suit card.suit)
+    (string_of_card_value card.value) ^ (string_of_card_suit card.suit)
 
 let create player_num =
     Printf.printf "Creating game with %d players\n" player_num;
